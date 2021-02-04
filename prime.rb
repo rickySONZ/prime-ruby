d23f6dc0aafb960c23f9f2e39ad7f11843155bc9
Ryan Erricson <ryanerricson@gmail.com>
2021-02-04 12:28:21 -0500

    commit tree a218213dcc04da0bdec65fe50884fa7911669961

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+    n = 2
+    answer = true
+    if num <= 1
+        answer = false
+    else
+        while n < num
+        answer = false if num % n == 0
+        n += 1
+    end
+end
+answer
+end
+  
